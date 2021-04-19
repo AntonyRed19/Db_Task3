4a151950378026f740ba8faf52a5216a8c679d1c
DESKTOP-GCCS405\Antony <anton.redy19@gmail.com>
2021-04-19 14:11:14 +0300

    added Employee

---
+++ Task3/Task3/Entities/Employee.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3.Entities
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime HiredDate { get; set; }
+        public DateTime DateofBirth { get; set; }
+        public int OfficeId { get; set; }
+        public int TitleId { get; set; }
+
+
+    }
+}
