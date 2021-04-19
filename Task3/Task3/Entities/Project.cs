1cc6cc8b3b39f3fc4960d764610b24c19c4c33eb
DESKTOP-GCCS405\Antony <anton.redy19@gmail.com>
2021-04-19 14:27:53 +0300

    added Project

--- Task3/Task3/Entities/Project.cs
+++ Task3/Task3/Entities/Project.cs
@@ -12,5 +12,6 @@ namespace Task3.Entities
         public string Name { get; set; }
         public decimal Badget { get; set; }
         public DateTime StartedDate { get; set; }
+        public List<EmployeeProject> Users { get; set; } = new List<EmployeeProject>();
     }
 }
